c27ebbbd9e9513e52cd478f33df2c62039b24b01
nayemk1999 <nayemk900@gmail.com>
2021-01-30 02:39:47 +0600

    upload Assignment- 4

---
+++ index.js
@@ -0,0 +1,55 @@
+// First Class Plus button
+document.getElementById("fClassPlusBtn").addEventListener("click", function () {
+    ticketHandler('f-class', true);
+})
+
+// First Class Minus Plus button
+document.getElementById("fClassMinusBtn").addEventListener("click", function () {
+    ticketHandler('f-class', false);
+})
+
+// Economy Class Plus button
+document.getElementById("eClassPlusBtn").addEventListener("click", function () {
+    ticketHandler('e-class', true);
+})
+
+// Economy Class Plus button
+document.getElementById("eClassMinusBtn").addEventListener("click", function () {
+    ticketHandler('e-class', false);
+});
+
+// Ticket Handler Part
+function ticketHandler(ticket, increase) {
+    const InputCount = document.getElementById(ticket + '-input');
+    const InputCountNumber = parseInt(InputCount.value);
+    let inputNewCount = InputCountNumber;
+    if (increase == true) {
+        inputNewCount = InputCountNumber + 1;
+    }
+    else if (increase == false && inputNewCount > 0) {
+        inputNewCount = InputCountNumber - 1;
+    }
+    InputCount.value = inputNewCount;
+    calculateTotalTicketPrice()
+}
+
+// Calculate Ticket Part
+function calculateTotalTicketPrice() {
+    const firstClassPriceNum = ticketCount("f-class")
+    const economyClassPriceNum = ticketCount("e-class")
+    totalPrice = firstClassPriceNum * 150 + economyClassPriceNum * 100; //Sub Total Price 
+    document.getElementById("subTotal").innerText = "$" + totalPrice;
+    const vat = Math.round(totalPrice * 0.10); //10% charge Vat Count
+    document.getElementById("vat").innerText = "$" + vat;
+    const grandTotal = totalPrice + vat; //Grand Total Ticket Price With vat
+    document.getElementById("grandTotal").innerText = "$" + grandTotal;
+}
+
+//Ticket Count-
+function ticketCount(className) {
+    const classPrice = document.getElementById(className + "-input");
+    const classPriceNum = parseInt(classPrice.value);
+    return classPriceNum;
+}
+
+//Thank You !
